c82ef04a9becba31908491369709c50e8e203d5e
BharathJawahar <bharathjawahar49@gmail.com>
2021-10-13 16:46:01 +0530

    Turn on bluetooth on starting of the app

--- app/src/main/java/com/example/socialdistancingbluetoothapp/MainActivity.java
+++ app/src/main/java/com/example/socialdistancingbluetoothapp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.socialdistancingbluetoothapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BluetoothAdapter mBluetoothAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if( !mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 99);
+        }
     }
 }
